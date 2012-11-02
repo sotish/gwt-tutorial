d2b1119cb4f7f03a9f045b593ad880bc44ac0f47
akgun <akgun.demirbas@gmail.com>
2012-11-02 21:18:20 +0200

    Initial commit.

---
+++ WebMessenger/src/com/sansaretti/client/WebMessenger.java
@@ -0,0 +1,16 @@
+package com.sansaretti.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class WebMessenger implements EntryPoint {
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	
+	}
+}
