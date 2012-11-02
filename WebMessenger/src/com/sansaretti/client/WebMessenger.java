ff437695f22e5d325809f8730978e7916ea68107
akgun <akgun.demirbas@gmail.com>
2012-11-02 21:53:41 +0200

    UserNamePanel olusturuldu. Bu panel uyenin kullanici adini girmesi icindir.

--- WebMessenger/src/com/sansaretti/client/WebMessenger.java
+++ WebMessenger/src/com/sansaretti/client/WebMessenger.java
@@ -1,16 +1,35 @@
 package com.sansaretti.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.sansaretti.client.ui.UserNamePanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class WebMessenger implements EntryPoint {
-	
+	UserNamePanel userNamePanel = null;
+
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-	
+		userNamePanel = new UserNamePanel(new UserNamePanel.GirisListener() {
+
+			@Override
+			public void girisDurumu(boolean durum) {
+				/*
+				 * giris durumu basarili ise
+				 */
+				if (durum) {
+					RootPanel.get().remove(userNamePanel);
+					Window.alert("giris basarili. Chat odasi gosterilecek.");
+					/* TODO sayfaya chat odasini yukle */
+				}
+			}
+		});
+
+		RootPanel.get().add(userNamePanel);
 	}
 }
---
+++ WebMessenger/src/com/sansaretti/client/ui/UserNamePanel.java
@@ -0,0 +1,86 @@
+package com.sansaretti.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class UserNamePanel extends Composite {
+	/**
+	 * ui.xml ile bu sinifin eslestirilmesi icin gerekli interface tanimi.
+	 */
+	interface UserNamePanelUiBinder extends UiBinder<Widget, UserNamePanel> {
+	}
+
+	/**
+	 * binder olustur.
+	 */
+	private static UserNamePanelUiBinder uiBinder = GWT
+			.create(UserNamePanelUiBinder.class);
+
+	/**
+	 * kullanicinin adini yazacagi text box
+	 */
+	@UiField
+	TextBox adTextBox;
+	/**
+	 * chat'e baslama butonu.
+	 */
+	@UiField
+	Button baslaButton;
+
+	public UserNamePanel(final GirisListener girisListener) {
+		/*
+		 * Composite sinifinin icerigini binder ile doldur.
+		 */
+		initWidget(uiBinder.createAndBindUi(this));
+
+		/*
+		 * basla tusuna tiklaninca
+		 */
+		baslaButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				/*
+				 * kullanici adini al
+				 */
+				String userName = adTextBox.getText();
+				/*
+				 * adi kontrol et
+				 */
+				if (userName == null || userName.isEmpty()) {
+					/*
+					 * kullaniciyi uyar.
+					 */
+					Window.alert("Lutfen adinizi giriniz.");
+					/*
+					 * UserNamePanel kullanicilarini uyar.
+					 */
+					girisListener.girisDurumu(false);
+					return;
+				}
+
+				/*
+				 * kullanici gecerli bir isim girdi. UserNamePanel
+				 * kullanicilarini uyar.
+				 */
+				girisListener.girisDurumu(true);
+			}
+		});
+	}
+
+	/**
+	 * Uyenin adini girip, basla butonuna basma durumu icin.
+	 * 
+	 */
+	public interface GirisListener {
+		void girisDurumu(boolean durum);
+	}
+}
